09b4543806fe503229cfc1a37f96bf0797f80526
Daniel Dean <daniel.dean414@gmail.com>
2019-11-20 18:22:47 -0700

    Cleaned up import a bit, and filled in README with file list and prompt

--- import_experimentation_etc.R
+++ import_experimentation_etc.R
@@ -3,12 +3,19 @@
 # Experimenting for now; probably delete in final version
 library(readr)
 library(dplyr)
+library(lubridate)
+library(stringr)
+
+
 
 homicides <- read_csv("https://raw.githubusercontent.com/washingtonpost/data-homicides/master/homicide-data.csv")
 #write_csv(homicides, "data/homicides-data.csv") #writing local copy
 
-  #names all seem fine already
-  
 
 homicides %>%
-  head()
+  mutate(reported_date = str_replace(string = reported_date, pattern = "201511018", replacement =  "20151018")) %>% #correcting apparent typos
+  mutate(reported_date = str_replace(string = reported_date, pattern = "201511105", replacement =  "20151018")) %>% #couldn't figure out in one step
+  mutate(reported_date = ymd(reported_date))
+
+  
+  
