1da6a7a230780e58676ad0c3b8586e886fc71f5c
Daniel Dean <daniel.dean414@gmail.com>
2019-11-20 17:50:47 -0700

    Added README and other ~setup documents

--- placeholder.R
+++ placeholder.R
@@ -1,1 +1,5 @@
 #placeholder file for HW5
+library(readr)
+
+homicides <- read_csv("https://raw.githubusercontent.com/washingtonpost/data-homicides/master/homicide-data.csv")
+write_csv(homicides, "data/homicides-data.csv")
