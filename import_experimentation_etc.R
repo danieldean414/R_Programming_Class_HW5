c6d55fb581ae8d292050db3aafb2b2e445b41982
Daniel Dean <daniel.dean414@gmail.com>
2019-11-21 13:33:58 -0700

    Trying to fix an issue with legend and fill/color. Saving updated rendered files as well

--- import_experimentation_etc.R
+++ import_experimentation_etc.R
@@ -9,8 +9,6 @@ library(tigris)
 library(forcats)
 library(sf)
 
-
-
 homicides <- read_csv("https://raw.githubusercontent.com/washingtonpost/data-homicides/master/homicide-data.csv") %>%
   mutate(reported_date = str_replace(string = reported_date, pattern = "201511018", replacement =  "20151018"),
          reported_date = str_replace(string = reported_date, pattern = "201511105", replacement =  "20151018"),
@@ -22,18 +20,17 @@ homicides <- read_csv("https://raw.githubusercontent.com/washingtonpost/data-hom
                 Solved = "Closed by arrest",
                Unsolved = c("Closed without arrest","Open/No arrest")
                ))
-
+#subsetting for Omaha
 homicides_map_omaha <- homicides %>%
   filter(city == "Omaha") %>%
   mutate(victim_race = fct_lump(victim_race, n=4, other_level = "Other")) %>%
+  add_count(victim_race, name = "race_count") %>%
+  mutate(victim_race = fct_reorder(victim_race, .x = race_count, .desc = TRUE)) %>%
   st_as_sf(coords = c("lon","lat")) %>%
   st_set_crs(4269)
 
-omaha_zip_codes <- tigris::zctas(starts_with = c("681"), tigris_cache_dir("data/")) %>%
-  st_as_sf(omaha_zip_codes) %>% st_set_crs(4269) 
-
-#All Omaha zip codes start with 681, but this also brings in a few non-Omaha zip codes to the south
-
-omaha_blocks <- block_groups()
-  
+#Importing census tracts:
+omaha_tracts <- tracts("NE", county = "Douglas") %>%
+  st_as_sf() %>%  
+  st_set_crs(4269)
   
